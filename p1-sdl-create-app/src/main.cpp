f1361072cdb1ec99c59b0340e53545f77473093f
sinhnx.dev <contact@sinhnx.dev>
2023-09-15 14:50:07 +0700

    update create background for windows and copy lib

--- p1-sdl-create-app/src/main.cpp
+++ p1-sdl-create-app/src/main.cpp
@@ -10,13 +10,26 @@ int main(int argv, char** args)
 		"SDL create Windows Demo", 
 		SDL_WINDOWPOS_CENTERED, 
 		SDL_WINDOWPOS_CENTERED,
-		800, 400, 0);
+		1116, 655, 0);
 	
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
 	bool isRunning = true;
 	SDL_Event event;
 
+	SDL_Surface* lettuce_sur = IMG_Load("assets/images/jungle.jpg");
+	if (lettuce_sur == NULL) {
+		std::cout << "Error loading image: " << IMG_GetError();
+		return 5;
+	}
+
+	SDL_Texture* background = SDL_CreateTextureFromSurface(renderer, lettuce_sur);
+	if (background == NULL) {
+		std::cout << "Error creating texture";
+		return 6;
+	}
+  SDL_FreeSurface(lettuce_sur);
+	
 	while (isRunning)
 	{
 		while (SDL_PollEvent(&event))
@@ -36,8 +49,8 @@ int main(int argv, char** args)
 		}
 
 		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-
+		SDL_SetRenderDrawColor(renderer, 0, 255, 255, 100);
+		SDL_RenderCopy(renderer, background, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
 
